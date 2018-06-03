46dbfb2711d7138d3170c8dda3ea3855645c6b48
j <j@example.com>
2018-06-03 13:08:02 +0700

    app-es6

---
+++ examples/demo-es6/src/index.js
@@ -0,0 +1,6 @@
+import DatGuiDefaults from '../../../lib/dat-gui-defaults'; // for dev
+//import DatGuiDefaults from '../../../dist/dat-gui-defaults';
+
+const dgd = new DatGuiDefaults({});
+console.log('999000');
+console.log('dgd:', dgd);
