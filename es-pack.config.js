6ed19770bfb9e44a7e4ed59260a9163b3fb5625e
j-devel <j@w3reality.com>
2020-03-29 16:36:44 +0700

    Use the `units` interface for more granular module verification

--- es-pack.config.js
+++ es-pack.config.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 module.exports = {
     onBundle: (webpackConfig) => {
     },
-    onVerify: (preloadJs) => {
+    onVerify: (preloadJs, units) => {
+        // Due to the `window` symbol in `dat.gui`, node-verify only these units
+        units.node.push('umd-require', 'esm-compat-require');
     },
 };
